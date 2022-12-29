f300967948247457d55a84c5ccc481b00b02e881
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-12-28 19:05:10 -0800

    feat: added animations to elements in about me view

--- pages/components/about.jsx
+++ pages/components/about.jsx
@@ -14,22 +14,22 @@ export default function About() {
                     {/* LIST OF LENGUAGES / FRAMEWORKS I HAVE WORKED WITH */}
                     <div className="flex w-full h-5/6 p-2">
                         {/* col 1 of skills */}
-                        <div className="flex flex-col w-1/2 font-bold h-full justify-between items-center">
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">HTML 5</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">CSS 3</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">REACT</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">NODE.JS</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">JAVASCRIPT</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">HEROKU</div>
+                        <div className="flex flex-col w-1/2 font-medium h-full justify-between items-center">
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">HTML 5</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">CSS 3</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">REACT</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">NODE.JS</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">JAVASCRIPT</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">HEROKU</div>
                         </div>
                         {/* col 2 of skills */}
-                        <div className="flex flex-col w-1/2 font-bold h-full justify-between items-center">
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">EXPRESS</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">NEXT.JS</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">TAILWIND</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">MONGO DB</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">BOOTSTRAP</div>
-                            <div className="hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">REST API</div>
+                        <div className="flex flex-col w-1/2 font-medium h-full justify-between items-center">
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">EXPRESS</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">NEXT.JS</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">TAILWIND</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">MONGO DB</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">BOOTSTRAP</div>
+                            <div className="hover:font-bold hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">REST API</div>
 
                         </div>
                     </div>
@@ -40,8 +40,8 @@ export default function About() {
                 </a>
                 {/* LINKS DOWNLOAD RESUME / LINKED-IN PROFILE */}
                 <div className="h-1/6 flex flex-row items-center w-full gap-2">
-                    <a className="font-bold text-xl w-1/2 flex items-center rounded-xl bg-customGreen text-white h-full p-2 text-center transition ease-in-out delay-100 hover:scale-[105%] hover:bg-customYellowLight hover:text-customBlack origin-center duration-500 hover:cursor-pointer">DOWNLOAD RESUME</a>
-                    <a className="font-bold text-xl w-1/2 flex items-center rounded-xl bg-customGreen text-white h-full p-2 text-center transition ease-in-out delay-100 hover:scale-[105%] hover:bg-customYellowLight hover:text-customBlack origin-center duration-500 hover:cursor-pointer">LINKED-IN PROFILE</a>
+                    <a className="font-bold text-lg w-1/2 flex items-center rounded-xl bg-customGreen text-white h-full p-2 text-center transition ease-in-out delay-100 hover:scale-[105%] hover:bg-customYellowLight hover:text-customBlack origin-center duration-500 hover:cursor-pointer">DOWNLOAD RESUME</a>
+                    <a className="font-bold text-lg w-1/2 flex items-center rounded-xl bg-customGreen text-white h-full p-2 text-center transition ease-in-out delay-100 hover:scale-[105%] hover:bg-customYellowLight hover:text-customBlack origin-center duration-500 hover:cursor-pointer">LINKED-IN PROFILE</a>
                 </div>
                 {/* QR & LINK TO GITHUB PROFILE */}
                 <a href="https://github.com/ethangalva" target="_blank" rel="noreferrer" className="h-1/6 bg-white rounded-xl text-customBlack flex p-4 flex-row items-center w-full hover:scale-[101%]">
