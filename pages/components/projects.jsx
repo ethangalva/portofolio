5db21058fd8f5dfdbc22dbb25197c0f773c66fd6
ethangalva <82258388+ethangalva@users.noreply.github.com>
2023-01-19 18:58:52 -0800

    feat(projects): added custom css clases for display
    New classes allow for the projects to be expanded/retracted based on a
    click in the main container if either one, as one expands the other
    retracts to make space for the other one.

--- pages/components/projects.jsx
+++ pages/components/projects.jsx
@@ -1,14 +1,19 @@
 import Image from "next/image"
 
 export default function Projects() {
+
+    const handleClick = event => { 
+        event.currentTarget.classList.toggle('active');
+    }
+
     return (
         <div className="h-screen flex flex-col justify-between px-8 gap-4 py-4 snap-center" id="projects">
-            <div className="flex flex-row flex-wrap items-center h-full">
+            <div className="flex flex-col flex-wrap items-center h-full relative overflow-hidden">
 
 
 
                 {/* SECTION FOR PROJECT 2 */}
-                <div className="w-full h-1/2 order-1 p-1">
+                <button onClick={handleClick} className="w-full h-1/2 order-1 p-1   project">
                     <div className="md:flex-row md:flex-wrap md:w-full w-full h-full p-2 flex flex-col justify-between rounded-xl text-customBlack bg-customYellow hover:bg-customPastelYellow transition ease-in-out delay-100 hover:scale-[101%] hover:bg-customYellowLight origin-center duration-500">
 
                         {/* TITLE AND SHORT DESCRIPTION OF PROJECT */}
@@ -21,19 +26,19 @@ export default function Projects() {
 
                             {/* thumbnail of websites */}
                             <div className="flex justify-center">
-                                <Image src="/../public/images/placeholder.png" className="w-full h-auto md:h-[180px] md:w-[180px] lg:h-5/6 lg:w-auto" width="200" height="200" alt="github profile qr code" />
+                                <Image src="/../public/images/placeholder.png" className="w-full h-auto md:h-[180px] md:w-[180px] lg:h-5/6 lg:w-auto max-w-[40vh]" width="200" height="200" alt="github profile qr code" />
                             </div>
 
                             {/* PROJECTS LINKS AND SEE MORE */}
-                            <a href="#" className="font-bold flex w-full justify-center">
-                                <p className="text-right hover:underline">SEE MORE</p>
-                            </a>
+                            <div className="font-bold flex md:hidden w-full justify-center">
+                                <p className="text-right hover:underline">CLICK TO SEE MORE</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </button>
 
                 {/* SECTION FOR PROJECT 2 */}
-                <div className="w-full h-1/2 order-2 p-1">
+                <div className="w-full h-1/2 order-1 p-1   project">
                     <div className="md:flex-row md:flex-wrap md:w-full w-full h-full p-2 flex flex-col justify-between rounded-xl text-customBlack bg-customGreen hover:bg-customPastelGreen transition ease-in-out delay-100 hover:scale-[101%] hover:bg-customYellowLight origin-center duration-500">
 
                         {/* TITLE AND SHORT DESCRIPTION OF PROJECT */}
@@ -45,7 +50,7 @@ export default function Projects() {
 
                             {/* thumbnail of websites */}
                             <div className="flex justify-center">
-                                <Image src="/../public/images/placeholder.png" className="w-full h-auto md:h-[180px] md:w-[180px] lg:h-5/6 lg:w-auto" width="200" height="200" alt="github profile qr code" />
+                                <Image src="/../public/images/placeholder.png" className="w-full h-auto md:h-[180px] md:w-[180px] lg:h-5/6 lg:w-auto max-w-[40vh]" width="200" height="200" alt="github profile qr code" />
                             </div>
 
                             {/* PROJECTS LINKS AND SEE MORE */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        customGreen: '#4C604C',
-        customBlack: '#28282B',
-        customYellow: '#ffc830',
-        customOrange: '#be5a38',
-        customPastelYellow: '#FFE15D',
-        customPastelGreen: '#557153',
-        customPastelOrange: '#FF884B',
-      },
-      fontFamily: {
-        handWritten: ['Just Another Hand', 'cursive']
-      }
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+        colors: {
+            customGreen: '#4C604C',
+            customBlack: '#28282B',
+            customYellow: '#ffc830',
+            customOrange: '#be5a38',
+            customPastelYellow: '#FFE15D',
+            customPastelGreen: '#557153',
+            customPastelOrange: '#FF884B',
+        },
+        fontFamily: {
+            handWritten: ['Just Another Hand', 'cursive']
+        },
+        
     },
-  },
-  plugins: [],
+    },
+    plugins: [],
 }
