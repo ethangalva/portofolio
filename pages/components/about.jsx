042aada30e610d024109b06669bddce79ebcd883
ethangalva <82258388+ethangalva@users.noreply.github.com>
2023-01-31 14:31:28 -0800

    feat(about): about.jsx is done, responsive
    Description: Need to add all of the links to all of the a html elements
    in all of the views.

--- pages/components/about.jsx
+++ pages/components/about.jsx
@@ -23,11 +23,11 @@ export default function About() {
 
 
                 {/* PERSONAL HOBIES */}
-                <div className="order-3 md:order-11 w-full h-3/6 md:p-1 md:w-3/6 gap-2 flex flex-col">
-                    <div className="w-full h-full bg-white rounded-xl flex items-center py-2      hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">
-                        <h3 className="w-full font-bold text-customBlack text-center px-2 md:text-xl hover:scale-[105%] duration-500 transition ease-in-out hover:cursor-default">Some of my hobies include reading and watching articles of javascript and css libraries. Experimenting with new frameworks and technologies is something that I really enjoy. </h3>
+                <div className="order-3 md:order-11 w-full h-3/6 md:p-1 md:w-3/6 lg:w-4/6 gap-2 flex flex-col">
+                    <div className="w-full h-full bg-white rounded-xl flex items-center py-2      hover:scale-[101%] duration-500 transition ease-in-out hover:cursor-default">
+                        <h3 className="w-full font-bold text-customBlack text-center px-2 md:text-xl hover:scale-[101%] duration-500 transition ease-in-out hover:cursor-default">Some of my hobies include reading and watching articles of javascript and css libraries. Experimenting with new frameworks and technologies is something that I really enjoy. </h3>
                     </div>
-                    <a className="font-bold text-2xl w-full h-2/6 bg-customGreen rounded-xl hidden md:flex px-2 text-center justify-center items-center gap-2">
+                    <a className="hover:underline hover:cursor-pointer font-bold text-2xl w-full h-2/6 bg-customGreen rounded-xl hidden md:flex px-2 text-center justify-center items-center gap-2">
                         SEND ME AN EMAIL
                         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-envelope-at-fill" viewBox="0 0 16 16">
                             <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
@@ -40,19 +40,19 @@ export default function About() {
                     <div className="w-full h-full flex flex-row md:flex-col items-center gap-2">
                         {/* LINKS DOWNLOAD RESUME / LINKED-IN PROFIL */}
                         <div className="order-1 w-1/2 h-full md:p-0 md:h-1/3 md:w-full                 transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
-                            <a href="" className="h-full bg-customYellow hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
+                            <a href="" className="h-full bg-customYellow hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2 hover:underline hover:cursor-pointer                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
                             {/* dot on the left side */}
-                                <div className="font-bold text-customBlack md:text-lg lg:text-2xl">GITHUB PROFILE</div>
+                                <div className="font-bold text-customBlack md:text-lg lg:text-2xl ">GITHUB PROFILE</div>
                             </a>
                         </div>
                         <div className="order-2 w-1/2 h-full md:p-0 md:h-1/3 md:w-full                  transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
-                            <a href="" className="h-full bg-customOrange hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
+                            <a href="" className="h-full bg-customOrange hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2 hover:underline hover:cursor-pointer                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
                             {/* dot on the left side */}
                                 <div className="font-bold text-customBlack md:text-lg lg:text-2xl">LINKED-IN PROFILE</div>
                             </a>
                         </div>
                         <div className="order-3 hidden md:p-0 md:h-1/3 md:flex md:w-full                  transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
-                            <a href="" className="h-full bg-customRed hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
+                            <a href="" className="h-full bg-customRed hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2 hover:underline hover:cursor-pointer                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
                             {/* dot on the left side */}
                                 <div className="font-bold text-customBlack md:text-lg lg:text-2xl">DOWNLOAD RESUME</div>
                             </a>
@@ -72,7 +72,7 @@ export default function About() {
                 </div>
 
                 {/* IMAGE OF ME ONLY IN MD AND OVER */}
-                <div className="order-8 w-3/6 h-3/6 md:p-1 hidden md:flex ">
+                <div className="order-8 w-3/6 h-3/6 lg:w-2/6 md:p-1 hidden md:flex      hover:scale-[101%] duration-500 transition ease-in-out hover:cursor-default ">
                     <div className="flex items-center justify-center w-full h-full bg-white rounded-xl">
                         <Image src="/../public/images/CuteAvataravatar.jpg" width="1000" height="1000" alt="github profile qr cod" className="w-5/6 md:w-auto h-5/6" />
                     </div>
