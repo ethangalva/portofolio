2a1b9ade72049e59b9ff37161bb508a0e3bdc477
ethangalva <82258388+ethangalva@users.noreply.github.com>
2023-02-01 15:52:12 -0800

    fix(resume): removed resume file and changed download of file to link

--- pages/components/about.jsx
+++ pages/components/about.jsx
@@ -50,9 +50,9 @@ export default function About() {
                             </a>
                         </div>
                         <div className="order-3 hidden md:p-0 md:h-1/3 md:flex md:w-full                  transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
-                            <a href="../../public/Ethan-Galvan.pdf" download className="h-full bg-customRed hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2 hover:underline hover:cursor-pointer                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
+                            <a href="https://my.indeed.com/p/ethang-a2fsdai" className="h-full bg-customRed hover:bg-customGray rounded-xl flex items-center flex-col text-center justify-center px-2 py-2 hover:underline hover:cursor-pointer                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
                             {/* dot on the left side */}
-                                <div className="font-bold text-customBlack md:text-lg lg:text-2xl">DOWNLOAD RESUME</div>
+                                <div className="font-bold text-customBlack md:text-lg lg:text-2xl">OPEN RESUME</div>
                             </a>
                         </div>
                     </div>
@@ -60,9 +60,9 @@ export default function About() {
 
                 {/* DOWNLOAD RESUME IN MD VIEW ONLY */}
                 <div className="order-2  md:hidden w-full h-1/6 md:w-full md:p-0                  transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
-                    <a href="../../public/Ethan-Galvan.pdf" download className="h-full bg-customOrange hover:bg-customGray rounded-xl flex items-center flex-row justify-center gap-4 px-2 py-2                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
+                    <a href="https://my.indeed.com/p/ethang-a2fsdai" className="h-full bg-customOrange hover:bg-customGray rounded-xl flex items-center flex-row justify-center gap-4 px-2 py-2                     transition ease-in-out delay-250 hover:scale-[101%] origin-center duration-500">
                         {/* dot on the left side */}
-                        <div className="font-bold text-customBlack text-lg lg:text-2xl">DOWNLOAD RESUME</div>
+                        <div className="font-bold text-customBlack text-lg lg:text-2xl">OPEN RESUME</div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="customBlack" className="bi bi-arrow-down-square-fill md:w-[30px] md:h-[30px]" viewBox="0 0 16 16">
                             <path d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
                         </svg>
